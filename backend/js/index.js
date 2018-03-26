7e9cbf01116b7fc2bc78b0226540ed3cb581560f
Martyna <chomicz.martyna@gmail.com>
2018-03-27 01:04:59 +0200

    Some refactor

--- backend/js/index.js
+++ backend/js/index.js
@@ -18,12 +18,21 @@ app.get('/countries', (req, res) => {
             code: country.ISO[2],
         }
     });
+
     res.send(result);
+
 });
 
 app.get('/countries/:code', (req, res) => {
     const countryCode = req.params.code;
     console.log(`Query for country with ${countryCode} code.`);
+
+    const error = getErrorResponseIfCountryDoesNotexist(countryCode);
+    if(error) {
+        res.send(error);
+        return;
+    }
+
     const countryInfo = countryjs.info(countryCode);
     res.send(countryInfo);
 });
@@ -31,6 +40,13 @@ app.get('/countries/:code', (req, res) => {
 app.get('/countries/:code/provinces', (req, res) => {
     const countryCode = req.params.code;
     console.log(`Query for provinces of country with ${countryCode} code.`);
+
+    const error = getErrorResponseIfCountryDoesNotexist(countryCode);
+    if(error) {
+        res.send(error);
+        return;
+    }
+
     const {provinces} = countryjs.info(countryCode);
     res.send(provinces);
 });
@@ -38,6 +54,13 @@ app.get('/countries/:code/provinces', (req, res) => {
 app.get('/countries/:code/wiki', (req, res) => {
     const countryCode = req.params.code;
     console.log(`Query for wiki redirection for country with ${countryCode} code.`);
+
+    const error = getErrorResponseIfCountryDoesNotexist(countryCode);
+    if(error) {
+        res.send(error);
+        return;
+    }
+
     const wiki = countryjs.wiki(countryCode);
     res.redirect(wiki);
 });
@@ -45,6 +68,13 @@ app.get('/countries/:code/wiki', (req, res) => {
 app.get('/countries/:code/center', (req, res) => {
     const countryCode = req.params.code;
     console.log(`Query for coordinates of country with ${countryCode} code. Tricky one ;-D`);
+
+    const error = getErrorResponseIfCountryDoesNotexist(countryCode);
+    if(error) {
+        res.send(error);
+        return;
+    }
+
     const geoJSON = countryjs.geoJSON(countryCode);
     const center = polygonCenter(geoJSON.features[0].geometry);
     res.send(center);
@@ -52,9 +82,15 @@ app.get('/countries/:code/center', (req, res) => {
 
 app.get('/countries/:code/weather/capital', (req, res) => {
     const countryCode = req.params.code;
-    const capital = countryjs.capital(countryCode);
+    console.log(`Query for weather in capital city of country with ${countryCode} code.`);
 
-    console.log(`Query for weather in ${capital} -capital city of country with ${countryCode} code.`);
+    const error = getErrorResponseIfCountryDoesNotexist(countryCode);
+    if(error) {
+        res.send(error);
+        return;
+    }
+
+    const capital = countryjs.capital(countryCode);
 
     let weatherQueryUrl = weatherUrl + `?q=${capital},${countryCode}&appid=${weatherKey}`;
     _getWeatherData(weatherQueryUrl)
@@ -71,80 +107,137 @@ app.get('/countries/:code/weather/capital', (req, res) => {
 });
 
 app.get('/weatherMath/temperatureMath', (req, res) => {
-    res.redirect(`/math?what=temp&countries=${req.query.countries}`);
-});
 
-app.get('/weatherMath/humidityMath', (req, res) => {
-    res.redirect(`/math?what=humidity&countries=${req.query.countries}`);
-});
-
-app.get('/weatherMath/pressureMath', (req, res) => {
-    res.redirect(`/math?what=pressure&countries=${req.query.countries}`);
-
-});
-
-app.get('/math', (req, res) => {
-    const what = req.query.what;
-    const countryCodes = req.query.countries;
-    console.log("Query for ", what, "analysis for countries with codes:", countryCodes);
-
-    if (!countryCodes) {
-        res.send("No country codes!")
+    const error = validateCountriesAndGetErrorResponse(req.query.countries);
+    if (error) {
+        res.send(error);
         return;
     }
 
-    const codesArr = countryCodes.split(",");
-
     (async () => {
-        const weatherDataAndCodesArray = await _getWeatherDataArray(codesArr);
-
-        let data = {};
-        let resObject = { data };
-
-        let maxData;
-        let minData;
-        weatherDataAndCodesArray.forEach(elem => {
-            data[elem.code] = elem.msg ? elem.msg : elem[what];
-            if (elem[what]) {
-                if (!maxData || elem[what] > maxData[what]) {
-                    maxData = elem;
-                }
-
-                if (!minData || elem[what] < minData[what]) {
-                    minData = elem;
-                }
-            }
-        });
-
-        if (maxData) {
-            resObject["countryMax" + what] = maxData.code;
-            resObject[what + "Max"] = maxData[what];
-        }
-
-        if (minData) {
-            resObject["countryMin" + what] = minData.code;
-            resObject[what + "Min"] = minData[what];
-        }
-
-        let sum = weatherDataAndCodesArray.reduce((prev, curr) => {
-            if (curr.temp) {
-                return prev + curr[what];
-            } else {
-                return prev;
-            }
-        }, 0);
-
-        resObject[what + "Avg"] = sum / weatherDataAndCodesArray.length;
-
-        res.send(resObject);
-        return;
+        res.send(await getJsonResponseForWhatCalculation("temp", req.query.countries));
     })();
 
 });
 
+app.get('/weatherMath/humidityMath', (req, res) => {
+
+    const error = validateCountriesAndGetErrorResponse(req.query.countries);
+    if (error) {
+        res.send(error);
+        return;
+    }
+
+    (async () => {
+        res.send(await getJsonResponseForWhatCalculation("pressure", req.query.countries));
+    })();
+
+});
+
+app.get('/weatherMath/pressureMath', (req, res) => {
+
+    const error = validateCountriesAndGetErrorResponse(req.query.countries);
+    if (error) {
+        res.send(error);
+        return;
+    }
+
+    (async () => {
+        res.send(await getJsonResponseForWhatCalculation("pressure", req.query.countries));
+    })();
+
+});
+
+/**
+ * returns json response with errorMessage if country with given code does not exist
+ * @param {*} code - code of country to check
+ */
+function getErrorResponseIfCountryDoesNotExist(code) {
+    const countryInfo = countryjs.info(code);
+    if (!countryInfo) {
+        return JSON.stringify({ errorMessage: `Country with ${code} code not found.` });
+    }
+}
+
+/**
+ * returns json response with errorMessage if any of given countries does not exist;
+ * errorMessage contains information only about first country which does not exist
+ * @param {*} codesString - string containing codes of countries to check separated by comma
+ */
+function validateCountriesAndGetErrorResponse(codesString) {
+
+    if(!codesString) {
+        return JSON.stringify({errorMessage: "No country codes provided."});
+    }
+
+    const codesArr = codesString.split(",");
+    for (code of codesArr) {
+        const error = getErrorResponseIfCountryDoesNotExist(code);
+        if (error) {
+            return error;
+        }
+    }
+}
+
+/**
+ * This function creates json response after doint some math for countries with given codes
+ * and using weather data given by what argument
+ * @param {*} what - can one one of three possible values: temp, humidity, pressure
+ * @param {*} countryCodes - valid country codes
+ */
+async function getJsonResponseForWhatCalculation(what, countryCodes) {
+    console.log("Query for ", what, "analysis for countries with codes:", countryCodes);
+
+    const codesArr = countryCodes.split(",");
+    const weatherDataAndCodesArray = await _getWeatherDataArray(codesArr);
+
+    let data = {};
+    let resObject = { data };
+
+    let maxData;
+    let minData;
+    weatherDataAndCodesArray.forEach(elem => {
+        data[elem.code] = elem.msg ? elem.msg : elem[what];
+        if (elem[what]) {
+            if (!maxData || elem[what] > maxData[what]) {
+                maxData = elem;
+            }
+
+            if (!minData || elem[what] < minData[what]) {
+                minData = elem;
+            }
+        }
+    });
+
+    if (maxData) {
+        resObject["countryMax" + what] = maxData.code;
+        resObject[what + "Max"] = maxData[what];
+    }
+
+    if (minData) {
+        resObject["countryMin" + what] = minData.code;
+        resObject[what + "Min"] = minData[what];
+    }
+
+    let sum = weatherDataAndCodesArray.reduce((prev, curr) => {
+        if (curr.temp) {
+            return prev + curr[what];
+        } else {
+            return prev;
+        }
+    }, 0);
+
+    resObject[what + "Avg"] = sum / weatherDataAndCodesArray.length;
+
+    return resObject;
+}
+
+
+
 app.listen(3000, () => {
     console.log(`Server up and running!`);
 });
+
 
 function _getWeatherData(weatherQueryUrl) {
     return fetch(weatherQueryUrl)
